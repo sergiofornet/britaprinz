e6d3bb9c7ca3df5ac4930c7e2eabfd9287276804
sergiofornet <sergio.fornet@gmail.com>
2021-01-08 09:27:05 +0100

    Add field validation

--- wp-content/plugins/britaprinz-custom-fields/includes/britaprinz-artist-fields.php
+++ wp-content/plugins/britaprinz-custom-fields/includes/britaprinz-artist-fields.php
@@ -9,7 +9,10 @@ function bp_artist_fields() {
 		->add_fields( array(
 			// Field::make( 'rich_text', 'bp_artist_bio', __( 'Biografía', 'britaprinz-custom-fields' ) ),
 			Field::make( 'text', 'bp_artist_order_name', __( 'Nombre para ordenar', 'britaprinz-custom-fields' ) )
-				->set_visible_in_rest_api( $visible = true ),
+				->set_visible_in_rest_api( $visible = true )
+				->set_attribute( 'pattern', '[a-z]+' )
+				->set_attribute( 'placeholder', 'sólo minúsculas, sin espacios ni caracteres especiales' )
+				->set_required(),
 		));
 
 	// add_filter( 'crb_media_buttons_html', function( $html, $field_name ) {

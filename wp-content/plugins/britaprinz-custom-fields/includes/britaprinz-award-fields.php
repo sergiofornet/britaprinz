6f6ba1a07a460211deb3f6b60f039d647bc79644
sergiofornet <sergio.fornet@gmail.com>
2021-01-14 12:23:08 +0100

    Add special edition fields on Award post type

--- wp-content/plugins/britaprinz-custom-fields/includes/britaprinz-award-fields.php
+++ wp-content/plugins/britaprinz-custom-fields/includes/britaprinz-award-fields.php
@@ -10,6 +10,24 @@ function bp_award_fields() {
 		->add_fields( array(
 			Field::make( 'rich_text', 'bp_award_history', __( 'Historia del premio', 'britaprinz-custom-fields' ) ),
 		) );
+	Container::make( 'post_meta', __( 'Edición especial', 'britaprinz-custom-fields' ) )
+		->where( 'post_type', '=', 'award' )
+		->add_fields( array(
+			Field::make( 'checkbox', 'bp_award_se_toggle', 'Edición especial' ),
+			Field::make( 'complex', 'bp_award_se', __( 'Edición especial' ) )
+				->set_conditional_logic( array(
+					array(
+						'field' => 'bp_award_se_toggle',
+						'value' => true,
+					)
+				) )
+				->add_fields( array(
+					Field::make( 'text', 'bp_award_se_year', __( 'Edición' ) )
+						->set_width( 25 ),
+					Field::make( 'rich_text', 'bp_award_se_winners', __( 'Ganadores' ) )
+						->set_width( 75 ),
+				) ),
+		) );
 	Container::make( 'post_meta', __( 'Detalles de la edición', 'britaprinz-custom-fields' ) )
 		->where( 'post_type', '=', 'award' )
 		->add_fields( array(
@@ -24,7 +42,7 @@ function bp_award_fields() {
 						->set_type( array( 'image' ) ),
 				) ),
 			Field::make( 'textarea', 'bp_award_mentions', __( 'Menciones de honor' ) ),
-			Field::make( 'textarea', 'bp_award_selected', __( 'Seleccionados' ) )
+			Field::make( 'textarea', 'bp_award_selected', __( 'Seleccionados' ) ),
 		) );
 	Container::make( 'post_meta', __( 'Edición', 'britaprinz-custom-fields' ) )
 		->where( 'post_type', '=', 'award' )
@@ -42,4 +60,11 @@ function bp_award_fields() {
 				->set_help_text( __( 'PDF', 'britaprinz-custom-fields' ) )
 				->set_type( array( 'application/pdf' ) )
 		));
+
+	add_filter( 'crb_media_buttons_html', function( $html, $field_name ) {
+		if ( $field_name === 'bp_award_se_winners' ) {
+			return;
+		}
+		return $html;
+	}, 10, 2);
 }
